a8d54bc2a992894d01c4cd1e1a2cf181092fe3dd
MunchingKitten <139931826+MunchingKitten@users.noreply.github.com>
2023-08-15 19:02:31 -0400

    Made UI little more functional and laid out better
    Need to put game ui under user score and put icon for picked item vs
    computer's item
    
    Need to set game end condition

--- script.js
+++ script.js
@@ -20,53 +20,61 @@ const getComputerChoice = () => {
 
 const game = () => {
     
-        userScore = 0;
-        computerScore = 0;
-        rounds = 0;
-    
-        const playRound = (weapon) => {
-            userSelection = weapon;
-            computerSelection = getComputerChoice();
-            const board = document.querySelector('#roundState');
-            const userScoreboard = document.querySelector('#userScore');
-            const computerScoreboard = document.querySelector('#computerScore');
+    userScore = 0;
+    computerScore = 0;
+    rounds = 0;
 
-            if (userSelection === 'Rock' && computerSelection === 'Scissors' || 
-                userSelection === 'Paper' && computerSelection === 'Rock' ||
-                userSelection === 'Scissors' && computerSelection === 'Paper') {
-                userScore+=1;
-                board.innerHTML = `You Win! ${userSelection} beats ${computerSelection}!`;
-            }
-            else if (userSelection === 'Rock' && computerSelection === 'Paper' || 
-                    userSelection === 'Paper' && computerSelection === 'Scissors' || 
-                    userSelection === 'Scissors' && computerSelection === 'Rock') {
-                computerScore+=1;
-                board.innerHTML = `You Lose! ${userSelection} beats ${computerSelection}!`;
-                    }
-            else {
-                board.innerHTML = `It's a tie!`;
-            }
-            rounds++;
-            userScoreboard.innerHTML = userScore;
-            computerScoreboard.innerHTML = computerScore;
+    const ui = document.querySelector('.gameUI');
+    ui.style.visibility = 'visible';
 
+    const board = document.querySelector('#roundState');
+    board.innerHTML = `Let the game begin!`;
+
+    const playRound = (weapon) => {
+        userSelection = weapon;
+        computerSelection = getComputerChoice();
+        const userScoreboard = document.querySelector('#userScore');
+        const computerScoreboard = document.querySelector('#computerScore');
+
+        if (userSelection === 'Rock' && computerSelection === 'Scissors' || 
+            userSelection === 'Paper' && computerSelection === 'Rock' ||
+            userSelection === 'Scissors' && computerSelection === 'Paper') {
+            userScore+=1;
+            board.innerHTML = `You Win! ${userSelection} beats ${computerSelection}!`;
         }
-        const pickRock = document.querySelector("#rock");
-        pickRock.addEventListener('click', () => playRound("Rock"));
-
-        const pickPaper = document.querySelector('#paper');
-        pickPaper.addEventListener('click', () => playRound("Paper"));
-
-        const pickScissors = document.querySelector('#scissors');
-        pickScissors.addEventListener('click', () => playRound("Scissors"));
-
-        if(userScore === 5) {
-            board.innerHTML = `You win the game! The world is saved!`
+        else if (userSelection === 'Rock' && computerSelection === 'Paper' || 
+                userSelection === 'Paper' && computerSelection === 'Scissors' || 
+                userSelection === 'Scissors' && computerSelection === 'Rock') {
+            computerScore+=1;
+            board.innerHTML = `You Lose! ${userSelection} beats ${computerSelection}!`;
+                }
+        else {
+            board.innerHTML = `It's a tie!`;
         }
-        else if (computerScore === 5) {
-            board.innerHTML = `You have lost. Thanks for nothing, scrub.`
+        rounds++;
+        userScoreboard.innerHTML = userScore;
+        computerScoreboard.innerHTML = computerScore;
 
+        if(userScore == 5) {
+            board.innerHTML = `You have won! The world is saved!`;
+            return
+            
         }
+        else if (computerScore == 5) {
+            board.innerHTML = `You have lost. Thanks for nothing, scrub.`;
+            return
+        }
+    }
+    const pickRock = document.querySelector("#rock");
+    pickRock.addEventListener('click', () => playRound("Rock"));
+
+    const pickPaper = document.querySelector('#paper');
+    pickPaper.addEventListener('click', () => playRound("Paper"));
+
+    const pickScissors = document.querySelector('#scissors');
+    pickScissors.addEventListener('click', () => playRound("Scissors"));
+
 } 
 
-game();
+const startBtn = document.querySelector('#startGame');
+startBtn.addEventListener('click', game);
